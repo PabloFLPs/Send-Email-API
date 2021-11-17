00f6c03179ca183ce65b29864a41a25e06c3df81
Pablo Felipe <pablofelipe1999@hotmail.com>
2021-11-17 20:09:41 -0300

    modifying 'index.js' to deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const nodemailer = require("nodemailer")
 
 const app = express()
-const PORT = 3030 || process.env.port
+const PORT = process.env.PORT || 3030
 
 const SMTP_CONFIG = require("./config/smtp")
 
